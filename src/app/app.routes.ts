daccdd4ca81c2eb2952b00b5697987ca94d8f3da
Lautaro Monserrat <lautaromonserrat@yahoo.com.ar>
2024-04-20 17:17:38 -0300

    inicio proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'quien-soy', component: QuienSoyComponent},
+];
