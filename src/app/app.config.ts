52d52fd2229d560c4d1e356f04104ea271d7c448
Lautaro Monserrat <lautaromonserrat@yahoo.com.ar>
2024-05-05 17:45:06 -0300

    configure .env

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,40 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tp-sala-de-juegos-2541e","appId":"1:284356451192:web:805eeb9ff321a4e53e868d","databaseURL":"https://tp-sala-de-juegos-2541e-default-rtdb.firebaseio.com","storageBucket":"tp-sala-de-juegos-2541e.appspot.com","apiKey":"AIzaSyBeErb_Kvi8Fjmkv4pJHazLtfJGsdAdUoI","authDomain":"tp-sala-de-juegos-2541e.firebaseapp.com","messagingSenderId":"284356451192"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"tp-sala-de-juegos-2541e","appId":"1:284356451192:web:805eeb9ff321a4e53e868d","databaseURL":"https://tp-sala-de-juegos-2541e-default-rtdb.firebaseio.com","storageBucket":"tp-sala-de-juegos-2541e.appspot.com","apiKey":"AIzaSyBeErb_Kvi8Fjmkv4pJHazLtfJGsdAdUoI","authDomain":"tp-sala-de-juegos-2541e.firebaseapp.com","messagingSenderId":"284356451192"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))
-  ]
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: environment.projectId,
+          appId: environment.appId,
+          databaseURL: environment.databaseURL,
+          storageBucket: environment.storageBucket,
+          apiKey: environment.apiKey,
+          authDomain: environment.authDomain,
+          messagingSenderId: environment.messagingSenderId,
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: environment.projectId,
+          appId: environment.appId,
+          databaseURL: environment.databaseURL,
+          storageBucket: environment.storageBucket,
+          apiKey: environment.apiKey,
+          authDomain: environment.authDomain,
+          messagingSenderId: environment.messagingSenderId,
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
