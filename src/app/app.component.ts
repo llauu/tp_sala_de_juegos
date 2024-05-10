2bcb986315fa06e4bd39ca5c7c174f0bb6094550
Lautaro Monserrat <lautaromonserrat@yahoo.com.ar>
2024-05-10 20:27:03 -0300

    chat terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet, NavigationEnd } from '@angular/router';
 import { AuthService } from './services/auth.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faComments, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { NgFor, formatDate } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Firestore, addDoc, collection, collectionData, orderBy, query } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule, NgFor, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,9 +21,15 @@ export class AppComponent {
   title = 'tp_sala_de_juegos';
   user: string | null | undefined = '';
   isLoggedIn: boolean = false;
+  showChatWindow: boolean = false;
   faUser = faUser;
+  faComment = faComments;
+  faTimes = faTimes;
+  messages: any[] = [];
+  messageText: string = '';
+  sub: Subscription | null = null;
 
-  constructor(public authService: AuthService, private router: Router) {
+  constructor(public authService: AuthService, private router: Router, private firestore: Firestore) {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.checkAuthState();
@@ -32,6 +42,7 @@ export class AppComponent {
       .then(res => {
         this.isLoggedIn = res;
         this.user = this.authService.getUser();
+        this.getMessages();
       })
       .catch(err => console.log(err));
   }
@@ -43,5 +54,57 @@ export class AppComponent {
         this.router.navigate(['/login'])
       })
       .catch(err => console.log(err));
+  }
+
+
+  toggleChatWindow() {
+    setTimeout(() => {
+      this.scrollToBottom();
+    }, 0);
+    this.showChatWindow = !this.showChatWindow;
+  }
+
+  sendMessage() {
+    if(this.messageText !== '') {
+      let col = collection(this.firestore, 'messages');
+      addDoc(col, {'date': new Date(), 'user': this.user, 'message': this.messageText});
+
+      this.messageText = '';
+    }
+    
+  }
+
+  getMessages() {
+    let col = collection(this.firestore, 'messages');
+    const orderQuery = query(col, orderBy('date', 'asc'));
+
+    this.messages = [];
+    const observable = collectionData(orderQuery);
+
+    this.sub = observable.subscribe((res: any) => {
+      this.messages = res;
+      
+      // Cada vez que haya un nuevo mensaje, envio el scroll hacia abajo de todo
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 0);
+    });
+  }
+
+  formatUser(user: string): string {
+    return user.split('@')[0];
+  }
+
+  formatTimestamp(timestamp: any): string {
+    const date = timestamp.toDate(); 
+    return formatDate(date, 'd MMM. y h:mm a', 'en-US'); 
+  }
+
+  
+  scrollToBottom() {
+    const chatMessages = document.getElementById('chatMessages');
+    if (chatMessages) {
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
   }
 }
